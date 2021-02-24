b2f662fb006b32fb88ea9536f7f589e15ca085e4
flb <f.lb@gmx.com>
2021-02-24 20:56:21 +0100

    change way to identify cfw

--- include/current_cfw.hpp
+++ include/current_cfw.hpp
@@ -3,6 +3,5 @@
 #include "constants.hpp"
 
 bool isServiceRunning(const char *serviceName);
-Result smAtmosphereHasService(bool *out, SmServiceName name);
 CFW getCFW();
 const extern CFW running_cfw;
