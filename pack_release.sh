3f7ecc7db8e45b9e4c5867029036975e7b93c571
flb <f.lb@gmx.com>
2021-03-27 01:44:44 +0100

    revert to personnal docker image

--- pack_release.sh
+++ pack_release.sh
@@ -5,6 +5,6 @@ cd $DIR
 rm -r switch/aio-switch-updater/
 mkdir -p switch/aio-switch-updater/
 cp aio-switch-updater.nro switch/aio-switch-updater/
-VERSION=$(grep "APP_VERSION :=" Makefile | cut -d' ' -f4)
-cp aiosu-forwarder/aiosu-forwarder.nro switch/aio-switch-updater/aio-switch-updater-v$VERSION.nro
+#VERSION=$(grep "APP_VERSION :=" Makefile | cut -d' ' -f4)
+#cp aiosu-forwarder/aiosu-forwarder.nro switch/aio-switch-updater/aio-switch-updater-v$VERSION.nro
 zip -FSr aio-switch-updater.zip switch/
