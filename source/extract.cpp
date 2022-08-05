6aa2b3fa77e77668aeda10e4a8552ccd487c5b81
PoloNX <geyrecharlie@gmail.com>
2022-08-06 00:15:05 +0200

    fix for pull request

--- source/extract.cpp
+++ source/extract.cpp
@@ -35,10 +35,35 @@ namespace extract {
         {
             return strcasecmp(a.c_str(), b.c_str()) == 0;
         }
+
+        void preWork(zipper::Unzipper& unzipper, const std::string& workingPath, std::vector<zipper::ZipEntry>& entries)
+        {
+            chdir(workingPath.c_str());
+            entries = unzipper.entries();
+            s64 uncompressedSize = 0;
+            s64 freeStorage;
+            for (const auto& entry : entries)
+                uncompressedSize += entry.uncompressedSize;
+
+            if (R_SUCCEEDED(fs::getFreeStorageSD(freeStorage))) {
+                if (uncompressedSize * 1.1 > freeStorage) {
+                    unzipper.close();
+                    brls::Application::crash("menus/errors/insufficient_storage"_i18n);
+                    std::this_thread::sleep_for(std::chrono::microseconds(2000000));
+                    brls::Application::quit();
+                }
+            }
+        }
     }  // namespace
 
     void extract(const std::string& filename, const std::string& workingPath, int overwriteInis, std::function<void()> func)
     {
+        zipper::Unzipper unzipper(filename);
+        std::vector<zipper::ZipEntry> entries;
+
+        preWork(unzipper, workingPath, entries);
+        unzipper.close();
+
         chdir(workingPath.c_str());
         unzFile zfile = unzOpen(filename.c_str());
         unz_global_info gi = {0};
@@ -46,6 +71,9 @@ namespace extract {
 
         ProgressEvent::instance().setTotalSteps(gi.number_entry);
         ProgressEvent::instance().setStep(0);
+
+        std::set<std::string> ignoreList = fs::readLineByLine(FILES_IGNORE);
+
 
         for (int i = 0; i < gi.number_entry; ++i) {
             char filename_inzip[0x301] = {0};
@@ -66,9 +94,24 @@ namespace extract {
                     outfile = fopen((filename_inzip_s + ".aio").c_str(), "wb");
                 }
 
+                if ((overwriteInis == 0 && filename_inzip_s.substr(filename_inzip_s.length() - 4) == ".ini") || std::find_if(ignoreList.begin(), ignoreList.end(), [&filename_inzip_s](std::string ignored){
+                                                                                                                    u8 res = ("/" + filename_inzip_s).find(ignored);
+                                                                                                                    return (res == 0 || res == 1); }) != ignoreList.end()) {
+                    if (!std::filesystem::exists("/" + filename_inzip_s)) {
+                        outfile = fopen((filename_inzip_s + ".aio").c_str(), "wb");
+                    } 
+                }
+
                 else {
                     outfile = fopen(filename_inzip, "wb");
+                    if (filename_inzip_s.substr(0, 13) == "hekate_ctcaer") {
+                        fs::copyFile("/" + filename_inzip_s, UPDATE_BIN_PATH);
+                        if (CurrentCfw::running_cfw == CFW::ams && util::showDialogBoxBlocking(fmt::format("menus/utils/set_hekate_reboot_payload"_i18n, UPDATE_BIN_PATH, REBOOT_PAYLOAD_PATH), "menus/common/yes"_i18n, "menus/common/no"_i18n) == 0) {
+                            fs::copyFile(UPDATE_BIN_PATH, REBOOT_PAYLOAD_PATH);
+                        }
+                    }   
                 }
+
                 for (int j = unzReadCurrentFile(zfile, buf, WRITE_BUFFER_SIZE); j > 0; j = unzReadCurrentFile(zfile, buf, WRITE_BUFFER_SIZE)) {
                     fwrite(buf, 1, j, outfile);
                 }
@@ -83,10 +126,7 @@ namespace extract {
         }
         
         unzClose(zfile);
-        remove(filename.c_str());
         ProgressEvent::instance().setStep(ProgressEvent::instance().getMax());
-
-        return;
     }
 
     std::vector<std::string> getInstalledTitlesNs()
